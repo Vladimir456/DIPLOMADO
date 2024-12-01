a4d79b1c4e1e05f1a646e65ce26572491a3bfa00
erick <naybra1994@gmail.com>
2024-12-01 12:42:01 -0400

    add ssl

--- src/database/database.js
+++ src/database/database.js
@@ -9,6 +9,15 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT, // or 'mysql' or 'postgres'
     logging: console.log, // set to true for logging DB queries
+
+    //PARAQUE FUNCIONE EN RENDER y PARA QUE FUNCIONE EN MODO LOCAL COMENTAR TODO EL DIALECT
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnauthorized: false,
+        }
+    }
 }
 );
+
 export default sequelize;
